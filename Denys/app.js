44bb897d8212fcc1f4290bf9c37c8b0fadc2228d
DenysMaiko <maikodenys@gmail.com>
2025-05-03 12:01:51 +0300

    EXPRESS HANDLERSgit status

--- Denys/app.js
+++ Denys/app.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use('/surprize', (req, res, next) => {
+    console.log('Middleware 1: Request received');
+    res.send('<h1>Kurwa Bobre!!</h1>');
+})
+
+app.use('/', (req, res, next) => {
+    console.log('Middleware 2: Processing request');
+    res.send('<h1>SLAVA UKRAINI!</h1>');
+})
+
 app.listen(3000);
